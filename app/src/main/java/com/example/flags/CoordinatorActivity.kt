8b00b08ce96cb49093849933c74e76fcc8a4b44e
DADJOD <kamu57@mail.ru>
2023-04-19 20:07:01 +0300

    commit_3

--- app/src/main/java/com/example/flags/CoordinatorActivity.kt
+++ app/src/main/java/com/example/flags/CoordinatorActivity.kt
@@ -2,6 +2,7 @@ package com.example.flags
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
@@ -10,6 +11,13 @@ class CoordinatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coordinator)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.main, menu)
+
+        return super.onCreateOptionsMenu(menu)
     }
 
     fun orderTrip(view: View) {
