dc13d702566aa5f6e1f61e729a677eb31d645b61
DADJOD <kamu57@mail.ru>
2023-04-23 18:44:30 +0300

    commit_9

--- app/src/main/java/com/example/flags/CoordinatorActivity.kt
+++ app/src/main/java/com/example/flags/CoordinatorActivity.kt
@@ -2,6 +2,7 @@ package com.example.flags
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -12,6 +13,24 @@ class CoordinatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coordinator)
+
+        registerForContextMenu(findViewById(R.id.cost))
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        if (v?.id == R.id.cost) {
+            menuInflater.inflate(R.menu.context, menu)
+        }
+        super.onCreateContextMenu(menu, v, menuInfo)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.context_menu_exit -> Toast.makeText(this, "Exit pressed", Toast.LENGTH_SHORT).show()
+            R.id.context_menu_copy -> Toast.makeText(this, "Copy pressed", Toast.LENGTH_SHORT).show()
+            R.id.context_menu_paste -> Toast.makeText(this, "Paste pressed", Toast.LENGTH_SHORT).show()
+        }
+        return super.onContextItemSelected(item)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -21,7 +40,7 @@ class CoordinatorActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.main_add -> Toast.makeText(this, "Add pressed", Toast.LENGTH_SHORT).show()
+            R.id.main_add    -> Toast.makeText(this, "Add pressed", Toast.LENGTH_SHORT).show()
             R.id.main_create -> Toast.makeText(this, "Create pressed", Toast.LENGTH_SHORT).show()
             R.id.main_delete -> Toast.makeText(this, "Delete pressed", Toast.LENGTH_SHORT).show()
         }
